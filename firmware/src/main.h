545c391d3a6a8db40fffe9a1c3ed12d4d11c22ec
Adam Coddington <me@adamcoddington.net>
2019-10-12 19:28:34 -0700

    Adding header file.

---
+++ firmware/src/main.h
@@ -0,0 +1,17 @@
+void cmdUnrecognized(const char*);
+void cmdGetPosition();
+void cmdToPosition();
+void cmdResetPosition();
+void cmdGetVoltage();
+
+void motorUp();
+void motorDown();
+void motorStop();
+
+void toPosition(int);
+void handleMotorPosition();
+void storePosition();
+
+void setup();
+void loop();
+void _loop();
